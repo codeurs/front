9c3bd6cd6f870a5505255736e8d4a0df57c416a1
Ben Merckx <ben@codeurs.be>
2018-03-28 16:28:00 +0200

    Add onroutechange to pages

--- src/ui/page.js
+++ src/ui/page.js
@@ -9,9 +9,23 @@ export class Page extends Component {
 		this.currentRoute = this.attrs.route
 	}
 
+	oninit(vnode) {
+		super.oninit(vnode)
+		this.onroutechange()
+	}
+
 	onafterupdate() {
 		this.currentRoute = this.attrs.route
 	}
+
+	onbeforeupdate = (vnode, old) => {
+		this.currentRoute = vnode.attrs.route
+		if (vnode.attrs.route.href == old.attrs.route.href)
+			this.onroutechange()
+		return true
+	}
+
+	onroutechange() {}
 
 	static render(attrs) {
 		return m(this, attrs)
