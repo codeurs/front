6bbb0312bbfc97e6223d8cd33900603d916b7f2e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-13 14:31:34 +0000

    Deploy codeurs/front to github.com/codeurs/front.git:gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = require('@codeurs/packer')(
-  'src/index.tsx', 'dist/bundle.js', {
+  'src/index.ts', 'dist/bundle.js', {
     include: [path.resolve('../dist')]
   }
 )
