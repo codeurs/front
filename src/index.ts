cdc9c081f15d4fb6751c6d8d5db53f980dbc2789
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-18 16:10:50 +0000

    Deploy codeurs/front to github.com/codeurs/front.git:gh-pages

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
 import './index.less'
-import m from 'mithril'
-import {View} from '../../dist'
+import m, {Children} from 'mithril'
+import {Component, Slider, SliderStore, classes} from '../../dist'
 
-class Examples extends View {
-	render() {
-		return m('h1', '@codeurs/front')
+class Examples extends Component {
+	slider = new SliderStore()
+
+	view() {
+		return [
+			m('h1', '@codeurs/front'),
+			m('.examples-slider',
+				{
+					oncreate: ({dom}) =>
+						this.slider.animating.map(toggle =>
+							dom.classList.toggle('is-animating', toggle)
+						)
+				},
+				m(Slider,
+					this.slider,
+					[1, 2, 3, 4, 5, 6, 7].map(slide =>
+						m('.examples-slider-slide',
+							classes({is: {animating: this.slider.animating()}}),
+							slide
+						)
+					)
+				)
+			)
+		]
 	}
 }
 
