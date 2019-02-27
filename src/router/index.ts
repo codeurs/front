fd55eedbed959b52fc7ca1e61c7fd1c874ccbe45
Ben Merckx <ben@codeurs.be>
2019-02-27 15:51:25 +0100

    Export Location and Matcher

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,9 @@
+import {Router} from './router'
+
 export * from './link'
 export * from './redirect'
 export * from './parseroute'
 export * from './route'
 export * from './switch'
-import {Router} from './router'
 export const HistoryRouter = Router
+export {Location, RouterContext, Matcher} from './router'
