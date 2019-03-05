db2a1231d8fe86d3916207ce015c801177c3474f
Ben Merckx <ben@codeurs.be>
2019-03-05 17:32:40 +0100

    Add onreset

--- src/container/autocomplete.ts
+++ src/container/autocomplete.ts
@@ -50,6 +50,7 @@ export class Autocomplete<Item> extends View<
 	{
 		store?: AutocompleteStore<Item>
 		onselect?: (item: Item) => void
+		onreset?: () => void
 		itemToString?: (item: Item) => string
 		children: AutocompleteRenderApi<Item>
 	},
@@ -246,7 +247,7 @@ export class Autocomplete<Item> extends View<
 	}
 
 	dispatch(action: AutocompleteAction<Item>, options: {async?: boolean} = {}) {
-		const {itemToString = (item: Item) => `${item}`, onselect} = this.attrs
+		const {itemToString = (item: Item) => `${item}`, onselect, onreset} = this.attrs
 		const {async} = options
 		const {selectedItem: wasSelected} = this.store
 		this.store.dispatch(action, {
@@ -255,6 +256,8 @@ export class Autocomplete<Item> extends View<
 		const {selectedItem} = this.store
 		if (wasSelected != selectedItem && selectedItem)
 			if (onselect) onselect(selectedItem)
+		if (wasSelected !== null && selectedItem === null)
+			if (onreset) onreset()
 		if (async) m.redraw()
 		else (m.redraw as any).sync()
 	}
