229ee51d93e41333c75d6f117d6cebe6a8d8a104
Ben Merckx <ben@codeurs.be>
2019-04-03 16:43:28 +0200

    Dropdown example

--- examples/src/views/autocomplete.ts
+++ examples/src/views/autocomplete.ts
@@ -3,10 +3,36 @@ import './autocomplete.less'
 import {Autocomplete, AutocompleteStore, classes, m, View} from '@codeurs/front'
 import countries from './countries'
 
+const dropdownOptions = [
+	'a', 'b', 'c'
+]
+
 export default class extends View {
 	autocomplete = new AutocompleteStore()
+	selectedDropdownItem: string
 	render() {
 		return m('div', [
+			m('h2', 'dropdown'),
+			m('', 
+				m(Autocomplete, {
+					onselect: (item?: string) => {
+						if (item) this.selectedDropdownItem = item
+					}
+				},
+					({isOpen, selectedItem, itemAttrs, menuAttrs, buttonAttrs, highlightedIndex}) =>
+						m('.dropdown', 
+							m('button.dropdown-label', buttonAttrs(), this.selectedDropdownItem || 'Select something'),
+							isOpen && m('.dropdown-menu', menuAttrs(),
+								dropdownOptions.map((option, index) =>
+									m('.dropdown-menu-item', itemAttrs({
+										item: option,
+										...classes({is: {active: highlightedIndex === index}})
+									}), option)
+								)
+							)
+						)	
+				)
+			),
 			m('h2', 'autocomplete'),
 			m('button', {
 				onclick: e => {
