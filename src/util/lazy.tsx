f2c02ec13e8c09ad6ea1e7a127e087b17f256e5a
Ben Merckx <ben@codeurs.be>
2019-06-11 15:33:39 +0200

    Use lazy cache

--- src/util/lazy.tsx
+++ src/util/lazy.tsx
@@ -2,24 +2,23 @@
 import {ComponentType, m} from '../hyperscript'
 import {View} from '../ui/view'
 
+const cache = new Map<Function, ComponentType<any>>()
+
 export const lazy = <T extends {}>(
 	fetch: () => Promise<{default: ComponentType<T>}>
 ) => {
 	return class LazyLoad<T> extends View<T> {
-		state: {
-			component?: ComponentType<T>
-		} = {}
-
 		onInit() {
+			if (cache.has(fetch)) return
 			fetch()
-				.then(component => this.setState({component: component.default}))
+				.then(component => cache.set(fetch, component.default))
+				.then(this.redraw)
 				.catch(console.error)
 		}
 
 		render() {
 			const {children, ...attrs} = this.attrs
-			const {component} = this.state
-			const Component = component as any
+			const Component = cache.get(fetch)
 			if (!Component) return null
 			return <Component {...attrs}>{children}</Component>
 		}
